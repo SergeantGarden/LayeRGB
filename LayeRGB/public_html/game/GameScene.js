93d6994c464adb11e7414bd4bed0466c30241988
Hugo Mater <Hugo.Mater@hva.nl>
2016-09-16 17:56:01 +0200

    Initial commit

---
+++ LayeRGB/public_html/game/GameScene.js
@@ -0,0 +1,39 @@
+/* 
+ *  Copyright 2016 Hugo Mater
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+function GameScene(engine)
+{
+    Scene.call(this);
+    
+    GameScene.prototype.Update = function(input, dt)
+    {
+        Scene.prototype.Update.call(this, input, dt);
+    };
+    
+    GameScene.prototype.Draw = function(context)
+    {
+        context.save();
+        context.fillStyle = "black";
+        context.fillRect(0,0, Engine.currentGame[engine.gameTitle].originalResolution.x, Engine.currentGame[engine.gameTitle].originalResolution.y);
+        context.restore();
+        
+        Scene.prototype.Draw.call(this, context);
+    };
+}
+
+GameScene.prototype = Object.create(Scene.prototype);
+
+
