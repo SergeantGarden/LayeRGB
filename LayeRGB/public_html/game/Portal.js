2d4aff165b881a7562c45a8f057f49b8cc63cf1b
Hugo Mater <Hugo.Mater@hva.nl>
2016-09-25 19:26:32 +0200

    Every portal now have their correct color.

--- LayeRGB/public_html/game/Portal.js
+++ LayeRGB/public_html/game/Portal.js
@@ -18,34 +18,72 @@ function Portal(layer, position, rotation, scale, sprite, moveable)
 {
     GameObject.call(this, position, rotation, scale, sprite, moveable);
     
-     var _portalLayer = layer;
-     var _callback = null;
+    var _portalLayer = layer;
+    var _callback = null;
      
-     Object.defineProperty(this, "layer", {
-         get: function() { return _portalLayer; }
-     });
+    Object.defineProperty(this, "layer", {
+        get: function() { return _portalLayer; }
+    });
      
-     Object.defineProperty(this, "callback", {
-         get: function() { return _callback; },
-         set: function(callback)
-         {
-             if(Engine.isFunction(callback))
-             {
-                 _callback = callback;
-             }
-         }
-     });
+    Object.defineProperty(this, "callback", {
+        get: function() { return _callback; },
+        set: function(callback)
+        {
+            if(Engine.isFunction(callback))
+            {
+                _callback = callback;
+            }
+        }
+    });
      
-     Portal.prototype.HandleCollision = function(other, side)
-     {
-         if(other instanceof Player)
-         {
-             if(this.callback !== null)
-             {
-                 this.callback.call();
-             }
-         }
-     };
+    Portal.prototype.Draw = function(ctx)
+    {
+       GameObject.prototype.Draw.call(this, ctx);
+
+       var color = "#0000FF";
+       var alpha = 0.75;
+       
+       switch(this.layer)
+       {
+           case LEVEL_LAYER.BLUE:
+                color = "#0000FF";
+                alpha = 0.75;
+                break;
+           case LEVEL_LAYER.RED:
+                color = "#FF0000";
+                alpha = 0.75;
+                break;
+           case LEVEL_LAYER.GREEN:
+                color = "#00FF00";
+                alpha = 0.5;
+                break;
+       }
+
+
+       ctx.save();
+       ctx.beginPath();
+       ctx.globalAlpha = alpha;
+       ctx.arc(this.position.x + 0.5,this.position.y + 0.5, 11, 0, 2 * Math.PI, false);
+       ctx.fillStyle = color;
+       ctx.fill();
+       ctx.globalAlpha = 1;
+       ctx.lineWidth = 2;
+       ctx.strokeStyle = color;
+       ctx.stroke();
+       ctx.closePath();
+       ctx.restore();
+    };
+     
+    Portal.prototype.HandleCollision = function(other, side)
+    {
+        if(other instanceof Player)
+        {
+            if(this.callback !== null)
+            {
+                this.callback.call();
+            }
+        }
+    };
 }
 
 Portal.prototype = Object.create(GameObject.prototype);
